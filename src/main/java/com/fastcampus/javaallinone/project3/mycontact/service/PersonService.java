84c22af23f96d9e61b7a6e11cb212fea08b5415a
kyunam Sim <ppp9026@naver.com>
2020-09-17 00:22:31 +0900

    @GetMapping 테스트코드 작성

--- src/main/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonService.java
+++ src/main/java/com/fastcampus/javaallinone/project3/mycontact/service/PersonService.java
@@ -23,26 +23,24 @@ public class PersonService {
     private BlockRepository blockRepository;
 
     public List<Person> getPeopleExcludeBlocks(){
-//        List<Person> people = personRepository.findAll();
-//
-//        return people.stream().filter(person -> person.getBlock() == null).collect(Collectors.toList());
         return personRepository.findByBlockIsNull();
     }
 
     public List<Person> getPeopleByName(String name){
-//        List<Person> people = personRepository.findAll();
-//
-//        return people.stream().filter(person -> person.getName().equals(name)).collect(Collectors.toList());
         return personRepository.findByName(name);
     }
 
     @Transactional
     public Person getPerson(Long id){
-        //Person person = personRepository.findById(id).get();
         Person person = personRepository.findById(id).orElse(null);
 
         log.info("person: {}", person);
 
         return person;
+    }
+
+    @Transactional
+    public void put(Person person){
+        personRepository.save(person);
     }
 }
