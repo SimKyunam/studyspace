052ba9431d617c0d9ce11543bd3699af4256340e
kyunam Sim <ppp9026@naver.com>
2020-10-03 00:05:49 +0900

    Maven에 H2 추가

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import org.slf4j.LoggerFactory;
 public class Main {
 
     private static Logger logger = LoggerFactory.getLogger(Main.class);
-    
+
     public static void main(String[] args) {
         logger.info("Hello world!");
     }
