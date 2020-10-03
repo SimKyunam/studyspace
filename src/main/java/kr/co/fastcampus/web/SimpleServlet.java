b8cc7079e8d65221e7a2ccdd97e4369c04ecd69d
kyunam Sim <ppp9026@naver.com>
2020-10-03 16:59:49 +0900

    Tomcat Pom.xml 추가 밑 메인설정

--- src/main/java/kr/co/fastcampus/web/SimpleServlet.java
+++ src/main/java/kr/co/fastcampus/web/SimpleServlet.java
@@ -1,6 +1,7 @@
 package kr.co.fastcampus.web;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +13,10 @@ import java.io.PrintWriter;
  * Blog : https://milenote.tistory.com
  * Github : https://github.com/SimKyunam
  */
+@WebServlet(
+        name="simple",
+        urlPatterns = "/simple"
+)
 public class SimpleServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
