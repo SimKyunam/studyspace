dd8c29480ac53ed88dfde7edd2134582a7295e95
kyunam Sim <ppp9026@naver.com>
2020-10-03 23:38:07 +0900

    스프링 bean설정

--- src/main/java/kr/co/fastcampus/cli/Main.java
+++ src/main/java/kr/co/fastcampus/cli/Main.java
@@ -2,6 +2,8 @@ package kr.co.fastcampus.cli;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.sql.*;
 
@@ -12,28 +14,9 @@ public class Main {
     public static void main(String[] args) throws ClassNotFoundException {
         logger.info("Hello world!");
 
-        Class.forName("org.h2.Driver");
-        var url = "jdbc:h2:mem:test;MODE=MySQL;";
-
-        try (var connection = DriverManager.getConnection(url, "sa", "");
-             var statement = connection.createStatement()){
-
-            connection.setAutoCommit(false);
-            statement.execute("create table member(id int auto_increment, username varchar(255) not null, password varchar(255) not null, primary key(id))");
-            try{
-                statement.executeUpdate("insert into member(username, password) values('simkyunam', '1234')");
-                connection.commit();
-            }catch (SQLException e){
-                connection.rollback();
-            }
-
-            var resultSet = statement.executeQuery("select id, username, password from member");
-            while (resultSet.next()) {
-                var member = new Member(resultSet);
-                logger.info(member.toString());
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        ApplicationContext context = new ClassPathXmlApplicationContext("dao.xml");
+        System.out.println(context);
+        Dao dao = context.getBean(Dao.class);
+        dao.run();
     }
 }
