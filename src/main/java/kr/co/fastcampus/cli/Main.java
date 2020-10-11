ff5ffe02b395a19484e23b65b90321f7df8fadb3
kyunam Sim <ppp9026@naver.com>
2020-10-11 23:31:11 +0900

    Spring IOC Configuration

--- src/main/java/kr/co/fastcampus/cli/Main.java
+++ src/main/java/kr/co/fastcampus/cli/Main.java
@@ -3,6 +3,7 @@ package kr.co.fastcampus.cli;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.sql.SQLException;
 
@@ -10,12 +11,9 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
         log.info("Hello world!");
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(AppConfig.class);
-        context.refresh();
-
-        B b = context.getBean(B.class);
-        log.info("" + b);
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("dao.xml");
+        Dao dao = context.getBean(Dao.class);
+        dao.run();
         context.close();
     }
 }
