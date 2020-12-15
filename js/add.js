6f872a9a418cd87ad1299faa303728245e5a580c
kyunam Sim <ppp9026@naver.com>
2020-12-15 22:50:39 +0900

    자바스크립트 연습 axios 사용

--- js/add.js
+++ js/add.js
@@ -1,91 +1,90 @@
-function getToken() {
-  return localStorage.getItem('token');
+function getToken(){
+    return localStorage.getItem('token');
 }
 
-async function getUserByToken(token) {
-  try {
-    const res = await axios.get('https://api.marktube.tv/v1/me', {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    return res.data;
-  } catch (error) {
-    console.log('getUserByToken error', error);
-    return null;
-  }
+async function getUserByToken(token){
+    try{
+        const res = await axios.get('https://api.marktube.tv/v1/me', {
+            headers:{
+                Authorization: `Bearer ${token}`
+            },
+        });
+        return res.data;
+    }catch(error){
+        console.log('getUserByToken error');
+        return null;
+    }
 }
 
-async function save(event) {
-  event.preventDefault();
-  event.stopPropagation();
-  event.target.classList.add('was-validated');
+async function save(event){
+    event.preventDefault();
+    event.stopPropagation();
+    console.log('save');
 
-  const titleElement = document.querySelector('#title');
-  const messageElement = document.querySelector('#message');
-  const authorElement = document.querySelector('#author');
-  const urlElement = document.querySelector('#url');
+    event.target.classList.add('was-validated');
 
-  const title = titleElement.value;
-  const message = messageElement.value;
-  const author = authorElement.value;
-  const url = urlElement.value;
+    const titleElement = document.querySelector('#title');
+    const messageElement = document.querySelector('#message');
+    const authorElement = document.querySelector('#author');
+    const urlElement = document.querySelector('#url');
 
-  if (title === '' || message === '' || author === '' || url === '') {
-    return;
-  }
+    const title = titleElement.value;
+    const message = messageElement.value;
+    const author = authorElement.value;
+    const url = urlElement.value;
 
-  const token = getToken();
-  if (token === null) {
-    location.href = '/login';
-    return;
-  }
+    if(title === '' || message ==='' || author ==='' || url ===''){
+        return;
+    }
 
-  try {
-    await axios.post(
-      'https://api.marktube.tv/v1/book',
-      {
-        title,
-        message,
-        author,
-        url,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      },
-    );
-    location.href = '/';
-  } catch (error) {
-    console.log('save error', error);
-    alert('책 추가 실패');
-  }
+    const token = getToken();
+    if(token === null){
+        location.assign('/login');
+        return;
+    }
+
+    try{
+        await axios.post('https://api.marktube.tv/v1/book', {
+            title,
+            message,
+            author,
+            url
+        }, {
+          headers:{
+              Authorization: `Bearer ${token}`
+          }  
+        });
+        location.assign('/');
+    }catch(error){
+        console.log('save error', error);
+    }
 }
 
-function bindSaveButton() {
-  const form = document.querySelector('#form-add-book');
-  form.addEventListener('submit', save);
+function bindSaveButton(){
+    const form = document.querySelector('#form-add-book');
+    form.addEventListener('submit', save);
 }
 
-async function main() {
-  // 버튼에 이벤트 연결
-  bindSaveButton();
+async function main(){
+    // 버튼에 이벤트 연결
+    bindSaveButton();
 
-  // 토큰 체크
-  const token = getToken();
-  if (token === null) {
-    location.href = '/login';
-    return;
-  }
+    // 토큰 체크
+    const token = getToken();
+    if(token === null){
+        location.assign('/login');
+        return;
+    }
 
-  // 토큰으로 서버에서 나의 정보 받아오기
-  const user = await getUserByToken(token);
-  if (user === null) {
-    localStorage.clear();
-    location.href = '/login';
-    return;
-  }
+    // 토큰으로 서버에서 나의 정보 받아오기
+    const user = await getUserByToken(token);
+    if(user === null){
+        localStorage.clear();
+        location.assign('/login');
+        return;
+    }
+
+    console.log(user);
 }
 
 document.addEventListener('DOMContentLoaded', main);
